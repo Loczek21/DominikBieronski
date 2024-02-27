3fef41f157a8f03b6f36f6020a5cfd182d021c05
Loczek21 <125195850+Loczek21@users.noreply.github.com>
2024-02-27 20:52:38 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+function checkPasswordStrength() {
+    var password = document.getElementById("password").value;
+    var strengthMeter = document.getElementById("strength-meter");
+    var passwordImage = document.getElementById("password-image");
+    var strength = 0;
+    if (password.match(/[a-z]+/)) {
+        strength += 1;
+    }
+    if (password.match(/[A-Z]+/)) {
+        strength += 1;
+    }
+    if (password.match(/[0-9]+/)) {
+        strength += 1;
+    }
+    if (password.length >= 8) {
+        strength += 1;
+    }
+    strengthMeter.innerHTML = "Siła hasła: " + strength + "/4";
+  
+    if (strength === 4) {
+        passwordImage.src = "haslo-silne.jpg";
+    } else if (strength >= 2) {
+        passwordImage.src = "haslo-srednie.jpg";
+    } else {
+        passwordImage.src = "haslo-slabo.jpg";
+    }
+}
